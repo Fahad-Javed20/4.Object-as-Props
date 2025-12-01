c385f20efb9ee5a48266e37d21e09ba341c2d370
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-12-02 01:35:09 +0500

    make changes

--- src/components/UserInformation.tsx
+++ src/components/UserInformation.tsx
@@ -7,11 +7,13 @@ interface UserInformationProps {
 const UserInformation = ({user}:UserInformationProps) => {
   return (
     <div className="h-100 w-80 bg-white shadow-lg/30 rounded-2xl">
-        <img className="h-64 w-full object-cover" src={user.image} alt="" />
+        <img className="h-64 w-full object-cover rounded-2xl" src={user.image} alt="" />
         <p>My Id:{user.id}</p>
         <h1>My Name:{user.name}</h1>
         <p>My age:{user.age}</p>
         {user.isMarried && <p>{user.name} is Still Single</p>}
+        <p>{user.contact.phone}</p>
+        <p>{user.contact.city}</p>
     </div>
   )
 }
--- src/types/UserType.ts
+++ src/types/UserType.ts
@@ -1,8 +1,11 @@
 export type UserType = {
-    id:number
-    image:string
-    name:string
-    age:number
-    isMarried:boolean
-
-}
+  id: number;
+  image: string;
+  name: string;
+  age: number;
+  isMarried: boolean;
+  contact: {
+    phone: string;
+    city: string;
+  };
+};
