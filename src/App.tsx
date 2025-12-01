307b03d3779f4aad1b8b556a90c82143dd855bee
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-12-02 01:14:54 +0500

    make chnages

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import UserInformation from './components/UserInformation'
+
+function App() {
+  
+  return (
+    <>
+      <UserInformation/>
+    </>
+  )
+}
+
+export default App
